daeafd1440879af590a5bb97db7f75eaa8421f07
mvallet91 <mvallet91@gmail.com>
2020-10-05 15:46:39 +0200

    Read only notepad

--- src/js/app/search/features/notepad/components/Notepad.js
+++ src/js/app/search/features/notepad/components/Notepad.js
@@ -4,9 +4,37 @@ import Iframe from "react-iframe";
 
 
 const Notepad = function(props) {
+
+    function getReadOnlyPad(padId){
+        const Http = new XMLHttpRequest();
+        let apiKey = '3a27994624a454bacdc02ba368ed25268f5b60079fa1429c0c9ac5d3b69c0abb'; // localManuel
+        apiKey = 'a7ffd005ad9357ee7b8b0fb1650a38a32a0638476a009d49cc0437e44ad01a85';
+
+        let url = 'http://localhost:9001/api/1.2.13/getReadOnlyID?apikey=' + apiKey + '&padID=' + padId + '?withCredentials=false'; // localManuel
+        url = 'http://lambda4.ewi.tudelft.nl/api/1.2.13/getReadOnlyID?apikey=' + apiKey + '&padID=' + padId ;
+
+        Http.open("GET", url, false);
+        Http.setRequestHeader("Content-Type", "text/plain");
+        Http.send();
+        let responseText = Http.responseText;
+        responseText = JSON.parse(responseText)
+        let readOnlyID = responseText['data']['readOnlyID']
+
+        return 'http://lambda4.ewi.tudelft.nl/ro/' + readOnlyID
+    }
+
+    function getPadUrl(padId){
+        let padUrl = "http://localhost:9001/p/" + padId + "?showControls=true&showChat=false&showLineNumbers=true&useMonospaceFont=false&withCredentials=true"; // localManuel
+        padUrl = "http://lambda4.ewi.tudelft.nl/p/" + padId + "?showControls=false&showChat=false&showLineNumbers=true&useMonospaceFont=false&withCredentials=false";
+
+        if (localStorage.getItem('post-test') === '1'){
+            padUrl = getReadOnlyPad(padId)
+        }
+        return padUrl
+    }
+
     const sidebarClass = props.isOpen ? 'sidebar open' : 'sidebar';
-    const padUrl = "http://lambda4.ewi.tudelft.nl/p/" + props.padUrl + "?showControls=false&showChat=false&showLineNumbers=true&useMonospaceFont=false&withCredentials=true";
-    // const padUrl = "http://localhost:9001/p/" + props.padUrl + "?showControls=true&showChat=false&showLineNumbers=true&useMonospaceFont=false&withCredentials=true";
+    const padUrl = getPadUrl(props.padUrl)
     return (
         <div className={sidebarClass}>
             <div>
